c3c88c35acfc81a90629c8ffd4bb36b5d6a8fed6
ykkc <ylane.admirable@gmail.com>
2020-02-08 11:29:22 +0900

    :sparkles: add project

---
+++ Grasshopper/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Grasshopper
+//
+//  Created by ykkc on 2020/02/08.
+//  Copyright © 2020 ykkc. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,36 @@
 # Grasshopper
+
+- 端末を管理するアレです
+
+# Environment
+
+## Develop Environment
+
+```
+- Xcode: 11.3.1
+- Swift: 5.1.3
+```
+
+## Project Build
+
+- N/A
+
+# Development Guideline
+
+## Testing Guideline
+
+- N/A
+
+## Libraries
+
+### CocoaPods
+
+- N/A
+
+### Carthage
+
+- N/A
+
+## CI/CD
+
+- N/A
